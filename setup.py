07072c7bf134da0ad83112fd4a3a0fe87f091e1f
RanjithK <ranjithk@multydeals.com>
2026-02-24 15:47:33 +0530

    requirements

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='medical_chatbot',
     version='0.1.0',
     author='Boktiar Ahmed Bappy',
-    author_email='entbappy73@gmail.com',
+    author_email='georgeranjithkumar07@gmail.com',
     packages=find_packages(),
     install_requires=[]
 )
